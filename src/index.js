8c7d8900aac1e17a3746bd97c2aa2c2164b0f633
Photoglyph <118303073+Photoglyph@users.noreply.github.com>
2022-12-14 15:20:07 -0600

    Create biocard function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ const root = document.querySelector('#root');
 
 
 const toDo = await toDoList.json();
+
+function createIdCard(bioCard){
+    return `
+    <h2>User ID ${bioCard.userId}</h2>
+    <div>ID Number ${bioCard.id}</div>
+    <div>Title ${bioCard.title}</div>
+    <div>Task Completed ${bioCard.completed}</div>
+    `;
+/* map it */
+
+}
